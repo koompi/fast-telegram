680e5f52ab13f5afbc483b010906363cf1fdfb92
LayNath242 <laynath242@gmail.com>
2020-07-02 14:47:29 +0700

    add poll message

--- app/models/message.py
+++ app/models/message.py
@@ -13,3 +13,70 @@ class GetMessage(BaseModel):
     from_user: Optional[str] = None
     ids: List[int] = None
     reverse: Optional[bool] = False
+
+
+class Contact(BaseModel):
+    phone_number: str
+    first_name: str
+    last_name: str
+    vcard: str
+    user_id: Optional[int]
+
+
+class Geo(BaseModel):
+    long: float
+    lat: float
+    access_hash: Optional[int]
+
+
+class Message(BaseModel):
+    text: str
+
+
+class Venue(Geo):
+    title: str
+    address: str
+    provider: str
+    venue_id: str
+    venue_type: str
+
+
+class Invoice(BaseModel):
+    title: str
+    description: str
+    currency: str
+    total_amount: Optional[int]
+    start_param: str
+    shipping_address_requested:	bool = None
+    test: bool = None
+    receipt_msg_id: Optional[int]
+
+
+class PollAnswer(BaseModel):
+    text: str
+    option: bytes
+
+
+class PollAnswerVoters(BaseModel):
+    option: bytes
+    voters: int
+    chosen:  Optional[bool]
+    correct:  Optional[bool]
+
+
+class Poll(BaseModel):
+    id: int
+    question: str
+    answers: List[PollAnswer]
+    closed: bool
+    public_voters: bool
+    multiple_choice: bool
+    quiz: bool
+    close_period: Optional[int]
+    close_date:	Optional[datetime]
+    min: Optional[bool]
+    results: List[PollAnswerVoters]
+    total_voters: Optional[int]
+    recent_voters: Optional[int]
+    solution: Optional[str]
+    # # solution_entities: List[MessageEntity]
--- app/utils/telegram/message.py
+++ app/utils/telegram/message.py
@@ -1,10 +1,20 @@
 from fastapi import HTTPException
-
+from typing import List
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 from ...core.config import api_id, api_hash
 from .entitiy import get_entity
+from ...models.message import (
+    Contact,
+    Geo,
+    Venue,
+    Invoice,
+    Message,
+    Poll,
+    PollAnswer,
+    PollAnswerVoters
+)
 
 
 async def get_all_messages(auth_key, msg):
@@ -70,35 +80,93 @@ async def get_all_messages(auth_key, msg):
 
 
 async def get_message_text(message):
-    try:
-        if message.contact:
-            phone_number = message.contact.phone_number
-            first_name = message.contact.first_name
-            last_name = message.contact.last_name
-            vcard = message.contact.vcard
-            user_id = message.contact.user_id
-
-            text = {
-                "message_id": user_id,
-                "phone_number": phone_number,
-                "name": f"{first_name} {last_name}",
-                "vcard":  vcard
-                }
-        elif message.text or message.raw_text:
-            text = message.text
-        else:
-            text = None
-    except Exception:
-        raise HTTPException(status_code=400, detail="Get message Error")
+    # try:
+    if message.contact:
+        text = Contact(
+            phone_number=message.contact.phone_number,
+            first_name=message.contact.first_name,
+            last_name=message.contact.last_name,
+            vcard=message.contact.vcard,
+            user_id=message.contact.user_id
+        )
+    elif message.venue:
+        text = Venue(
+            long=message.venue.geo.long,
+            lat=message.venue.geo.lat,
+            access_hash=message.venue.geo.access_hash,
+            title=message.venue.title,
+            address=message.venue.address,
+            provider=message.venue.provider,
+            venue_id=message.venue.venue_id,
+            venue_type=message.venue.venue_type
+        )
+    elif message.geo:
+        text = Geo(
+            long=message.geo.long,
+            lat=message.geo.lat,
+            access_hash=message.geo.access_hash
+        )
+    elif message.invoice:
+        invoice = message.invoice
+        text = Invoice(
+            title=invoice.title,
+            description=invoice.description,
+            currency=invoice.currency,
+            total_amount=invoice.total_amount,
+            start_param=invoice.start_param,
+            shipping_address_requested=invoice.shipping_address_requested,
+            test=invoice.test,
+            receipt_msg_id=invoice.receipt_msg_id
+        )
+    elif message.poll:
+        poll = message.poll
+        answers: List[PollAnswer] = []
+        for answer in poll.poll.answers:
+            answers.append(
+                PollAnswer(
+                    text=answer.text,
+                    option=answer.option
+                )
+            )
+        results: List[PollAnswerVoters] = []
+        for result in poll.results.results:
+            results.append(
+                PollAnswerVoters(
+                    option=result.option,
+                    voters=result.voters,
+                    chosen=result.chosen,
+                    correct=result.correct
+                )
+            )
+        text = Poll(
+            id=poll.poll.id,
+            question=poll.poll.question,
+            answers=answers,
+            closed=poll.poll.closed,
+            public_voters=poll.poll.public_voters,
+            multiple_choice=poll.poll.multiple_choice,
+            quiz=poll.poll.quiz,
+            close_period=poll.poll.close_period,
+            close_date=poll.poll.close_date,
+            min=poll.results.min,
+            results=results,
+            total_voters=poll.results.total_voters,
+            recent_voters=poll.results.recent_voters,
+            solution=poll.results.solution,
+        )
+    elif message.text:
+        text = Message(
+            text=message.text
+        )
+    else:
+        text = None
+    # except Exception:
+    #     raise HTTPException(status_code=400, detail="Get message Error")
     return text
 
 
 def get_file(message):
-    if message.geo:
-            file = "geo type (unsupport)"
-    elif message.venue:
-        file = "venue type (unsupport)"
-    elif message.invoice:
+    if message.invoice:
         file = "invoice type (unsupport)"
     elif message.poll:
         file = "poll type (unsupport)"

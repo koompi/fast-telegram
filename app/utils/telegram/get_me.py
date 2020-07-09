32d8fd4112b568361292d4cbe7530f89eabfb043
LayNath242 <laynath242@gmail.com>
2020-07-09 17:28:50 +0700

    fix

--- app/utils/telegram/get_me.py
+++ app/utils/telegram/get_me.py
@@ -14,8 +14,8 @@ async def get_username(auth_key):
         raise HTTPException(status_code=400, detail="Failed to connect")
 
     me = await client.get_me()
-    if not me.username:
+    if me.username is None:
         username = ""
     else:
-        username = ""
+        username = me.username
     return username
